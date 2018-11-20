e5353d5ecc1af983b66929f1b5a834a660c72a20
marcingal <marcin921125@gmail.com>
2018-11-20 12:25:46 +0100

    refactored code

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const Button = (props) => (
-    <button>{props.label}</button>
+const Button = ({ label }) => (
+    <button>{label}</button>
 )
 
 export default Button
