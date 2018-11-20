e6600310a7c40b07b614276ccf1ec3dbdcc8e564
marcingal <marcin921125@gmail.com>
2018-11-20 12:24:53 +0100

    buttons added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React from 'react'
+import Button from './Button'
 
 const names = ['Ala', 'Ola', 'Ela']
-
+const nameList = names.map((element, i) => { return <li key={element}>{element}</li> })
 const App = (props) => (
   <div>
     <div>{names}</div>
     <ul>
       {
-        names.map((element, i) => {
-          return <li>{element}</li>
-        })
+        nameList
       }
     </ul>
+    <div>
+      <Button
+        label="Click me!"
+      />
+    </div>
   </div>
 )
 export default App
