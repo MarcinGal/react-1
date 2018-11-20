136da51dc38fc09dbf7aafc456ab8e41dc84f81c
marcingal <marcin921125@gmail.com>
2018-11-20 14:29:33 +0100

    button exact added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+import Button from './Button'
 import Route1 from './Route1'
 import Route2 from './Route2'
 import Route3 from './Route3'
@@ -9,6 +10,7 @@ const App = (props) => (
   <div>
     <BrowserRouter>
       <div>
+        <Route path="/" exact={true} component={() => <Button label="button text"/>} />
         <Route path="/route-1" component={Route1} />
         <Route path="/route-2" component={Route2} />
         <Route path="/route-3" component={Route3} />
